f87882089856dfe9c40ff4a7efda0905b07e601c
parassyam <84825042+parassyam@users.noreply.github.com>
2023-10-26 22:34:55 +0700

    Update index.js
    menambahkan percabangan ("Nilai Tidak Sesuai :")

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ let nilaiAngka = 73;
 console.log("Nilai Angka : "+nilaiAngka);
 
 //membuat percabangan dari nilaiAngka
-if (nilaiAngka >= 85){
+if (nilaiAngka >= 85 && nilaiAngka <=100){
   console.log("Nilai Huruf : A");
-} else if (nilaiAngka >=75){
+} else if (nilaiAngka >=75 && nilaiAngka <=84){
   console.log("Nilai Huruf : B");
-} else if (nilaiAngka >=60){
+} else if (nilaiAngka >=60 && nilaiAngka <=74){
   console.log("Nilai Huruf : C");
+  } else if (nilaiAngka >=0 && nilaiAngka <=59){
+    console.log("Nilai Huruf : D");
 } else {
-  console.log("Nilai Huruf : D");
+  console.log("Nilai Tidak Sesuai.");
 }
 
 //menggunakan modulo(%) atau sisa bagi dari 73 dibagi 2, lalu hasilnya di bandingkan dengan 0
@@ -24,4 +26,3 @@ if (nilaiAngka % 2 == 0) {
 } else {
   console.log("Bilangan Ganjil");
 }
-
